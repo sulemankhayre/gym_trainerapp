1a7377ae4bda1f41cf93c0f881f63d4867a818f3
sulemankhayre <97274079+sulemankhayre@users.noreply.github.com>
2022-05-16 20:59:57 +1000

    services-done

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,18 +1,43 @@
 class ServicesController < ApplicationController
   skip_before_action :verify_authenticity_token
 
-  def new 
-    @service = Service.new
-  end 
-  def create
-    service = @Service.create!(service_params)
-    redirect_to @trainer
+  def return_trainer
+  @trainer_id = current_user.trainer.id
   end
+  def index
+  end 
+  def show
+    @service = Service.find(params[:id])
+  end
+  def create
+    @service = Service.new(service_params)
+    @service.trainer_id = current_user.id
+    @service.save!
+    redirect_to @service
+  end
+  def edit
+    @service = Service.find(params[:id])
+  end 
+   def update 
+    @service = Service.find(params[:id])
+    @service.update(service_edit_params)
+    redirect_to @service
+  end 
 
+  def destroy 
+    @service = Service.find(params[:id])
+    @service.destroy
+    redirect_to @service
+
+  end
 
   private 
 
     def service_params
+    return params.permit(:product, :description,:location )
+  end 
+
+  def service_edit_params
     return params.require(:service).permit(:product, :description,:location )
   end 
 
